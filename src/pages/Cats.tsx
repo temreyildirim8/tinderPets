fd1bc2d30245eece6818e90bcbf11d1c9b511f31
Emre Yildirim <temreyildirim8@gmail.com>
2023-03-03 00:35:39 +0300

    Correct the layout

--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -129,13 +129,14 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     width: '100%',
-    height: 200,
+    height: 450,
     marginBottom: 20,
+    borderRadius: 50,
   },
   image: {
     width: '100%',
-    height: 200,
-    borderRadius: 20,
+    height: '100%',
+    borderRadius: 50,
   },
   imageTextContainer: {
     position: 'absolute',
